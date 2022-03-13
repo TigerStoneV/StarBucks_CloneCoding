d62fd7915b492a93c9b9cf49de11cb5621ff34af
TigerStoneV <https://github.com/ddang_gu@naver.com>
2022-03-13 22:55:35 +0900

    clear:StarBucks

--- js/main.js
+++ js/main.js
@@ -15,25 +15,43 @@ searchInputEl.addEventListener('blur',function(){
 });
 
 // scroll 될때마다 숫자가 늘어남
-const badgeEl = document.querySelector('header .badges');
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
 
-window.addEventListener('scroll',_.throttle(function(){
-    console.log(window.scrollY);
-    if(window.scrollY > 500){
-        // badge 숨기기
-        // gsap.to(요소 지속시간 옵션) 많이 씀 js animation library 
-        gsap.to(badgeEl, .6 , {
-            opacity:0,
-            display: 'none'
-        })
-    }else {
-        // badge 나타내기
-        gsap.to(badgeEl, .6 , {
-            opacity:1,
-            display: 'block'
-        })
-    }
-},300));
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
+  })
+})
 // _.throttle(함수,시간) 부화 주기. 
 // 300 은 0.3s 의미 하는데 0.3초 부하를 줘서 동시에 함수 호출시 충돌 방지
 
@@ -69,6 +87,18 @@ new Swiper('.promotion .swiper-container', {
         nextEl:'.promotion .swiper-next'
     }
 });
+new Swiper('.awards .swiper-container', {
+    // direction: 'horizontal', // 수평 슬라이드
+    autoplay: true, // 자동 재생 여부
+    loop: true, // 반복 재생 여부
+    spaceBetween: 30, // 슬라이드 사이 여백
+    slidesPerView: 5, // 한 번에 보여줄 슬라이드 개수
+    // slidesPerGroup: 5, // 한 번에 슬라이드 할 개수(전체 개수로 나뉘어야 함)
+    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+      prevEl: '.awards .swiper-prev', // 이전 버튼 선택자
+      nextEl: '.awards .swiper-next' // 다음 버튼 선택자
+    }
+  })
 
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
@@ -116,4 +146,9 @@ spyEls.forEach(function(spyEl){
 });
 
 
+/**
+ * 올해가 몇 년도인지 계산
+ */
+ const thisYear = document.querySelector('.this-year')
+ thisYear.textContent = new Date().getFullYear()
 
